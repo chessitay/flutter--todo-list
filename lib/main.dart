78684409933d95f8aa376d95ce5713a001272530
chessitay <ng842009@big.atid.org.il>
2026-01-07 12:57:35 +0200

    made the main theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF000080)),
+      ),
+
       home: Scaffold(
-        body: const SafeArea(
+        backgroundColor: const Color(0xFF000080),
+        body: SafeArea(
           child: Padding(
             padding: EdgeInsets.all(20.0),
-            child: Column(
+            child: Column(  
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                ),
                 Text(
                   'Todo List',
                   style: TextStyle(
                     fontSize: 32,
                     fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                ),
-              ],
+                  ),
+                ],
             ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          backgroundColor: Colors.deepPurple,
+          backgroundColor: Colors.blue,
           foregroundColor: Colors.white,
           child: const Icon(Icons.add),
         ),
